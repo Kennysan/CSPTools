8181ecf66a52a7c97fefdf6446a62c7b65b76b73
Kenneth Lee <kennysan@gmail.com>
2013-08-12 01:41:59 -0400

    The parser will now output a CSP policy header name by default

--- parser/run.py
+++ parser/run.py
@@ -7,10 +7,16 @@ parser = argparse.ArgumentParser(description="Log Parser")
 parser.add_argument('log', metavar='logfile', help='log file to parse', type=argparse.FileType('r'))
 parser.add_argument('-r', '--ignoreproto', help='Ignore protocols in CSP', action='store_true')
 parser.add_argument('-o', '--host', help='Hostname of the site')
+parser.add_argument('-n', '--noheader', help='Just output the policy, sans header', action='store_true')
+
 args = parser.parse_args()
 
 cspparser = CSPParser(args.host)
 cspparser.load(args.log.readlines())
 args.log.close()
 cspparser.generate(args.ignoreproto)
+if args.noheader:
+    pass
+else:
+    print("Content-Security-Policy-Report-Only:"),
 print cspparser
